948b6e6e983ce146ace5e84c4e12ffc1bdb23fe6
run3er <run3er@gmail.com>
2017-04-23 23:56:34 +0100

    Allow only GET on updates.

--- src/main/java/projMngmntSaaS/repositories/ProjectLevelUpdateRepository.java
+++ src/main/java/projMngmntSaaS/repositories/ProjectLevelUpdateRepository.java
@@ -2,6 +2,7 @@ package projMngmntSaaS.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import projMngmntSaaS.domain.entities.projectLevel.ProjectLevelUpdate;
 
 import java.util.UUID;
@@ -13,4 +14,18 @@ import java.util.UUID;
 public interface ProjectLevelUpdateRepository extends JpaRepository<ProjectLevelUpdate, UUID>
 {
     // Registering repository implementation automatically via Spring Data Rest
+
+    // Forbid POST, PUT & DELETE methods, allow only GET
+
+    @Override
+    @RestResource(exported = false)
+    void delete(UUID uuid);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Iterable<? extends ProjectLevelUpdate> entities);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ProjectLevelUpdate> S save(S entity);
 }
