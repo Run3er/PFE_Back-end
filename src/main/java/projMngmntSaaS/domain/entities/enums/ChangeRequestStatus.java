cde4969efe0770f81a49fdb999b8e06e82f82b31
run3er <run3er@gmail.com>
2017-04-25 22:35:05 +0100

    Full-fledged ChangeRequest & PendingIssue entities.

---
+++ src/main/java/projMngmntSaaS/domain/entities/enums/ChangeRequestStatus.java
@@ -0,0 +1,14 @@
+package projMngmntSaaS.domain.entities.enums;
+
+/**
+ * Possible {@link projMngmntSaaS.domain.entities.projectLevel.artifacts.ChangeRequest} status values.
+ */
+public enum ChangeRequestStatus
+{
+    STUDYING,
+    ONGOING,
+    ACCEPTED,
+    REFUSED,
+    ABANDONED,
+    REPLACED
+}
